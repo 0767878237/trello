54dccefa1219cb13cdb90f019390fd23356df5c6
Le Duc Anh Tuan <93181556+0767878237@users.noreply.github.com>
2025-04-16 10:55:08 +0700

    Delete odoo-18.0/custom_addons/task_management directory

--- odoo-18.0/custom_addons/task_management/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import task_tag
-from . import task_group
-from . import task
--- odoo-18.0/custom_addons/task_management/models/task_tag.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models, _
-
-class TaskTag(models.Model):
-    _name = 'task.tag'
-    _description = 'Task Tag'
-
-    name = fields.Char('Name', required=True)
-    color = fields.Integer('Color Index')
